65ca6bd4eec398d76be4237510ae45692f1323d7
Lucas Oliveira <lucasvmo42@gmail.com>
2025-01-21 15:14:41 -0300

    método de cadastro de usuario implementado no front-end

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -1,17 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <div className="w-full flex justify-center py-4 bg-indigo-800 text-white">
         <div className="container flex justify-between text-lg">
-          Blog Pessoal
+          <Link to="/" className="text-2xl font-bold">
+            Blog Pessoal
+          </Link>{" "}
           <div className="flex gap-4">
-            <div className="hover:underline">Postagens</div>
-            <div className="hover:underline">Temas</div>
-            <div className="hover:underline">Cadastrar</div>
-            <div className="hover:underline">Perfil</div>
-            <div className="hover:underline">Sair</div>
+            <div className="hover:underline">
+              <Link to="/">Postagens</Link>
+            </div>
+
+            <div className="hover:underline">
+              <Link to="/">Temas</Link>
+            </div>
+            <div className="hover:underline">
+              <Link to="/cadastro"> Cadastro</Link>
+            </div>
+            <div className="hover:underline">
+              <Link to="/home">Perfil</Link>
+            </div>
+            <div className="hover:underline">
+              <Link to="/">Sair</Link>
+            </div>
           </div>
         </div>
       </div>
